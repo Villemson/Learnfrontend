96865c90782709b7c02ec2901bbfa68dc7501b4b
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-02-01 14:36:25 +0200

    Remove nunjucks

--- src/index.js
+++
@@ -1,5 +0,0 @@
-console.log('Hello Webpack');
-const common = require('./common.js');
-common.hello();
-import es from './es.js';
-es.hello();
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-let pages = [];
-for (let i = 0; i < 10; i++) {
-    pages.push(new HtmlWebpackPlugin({
-        filename: i + '.html',
-        template: './src/number.njk',
-        templateParameters: {
-            number: i
-        }
-    }));
-}
-
-
 module.exports = {
     //   mode: 'development',
     entry: './src/index.js',
@@ -30,25 +18,12 @@ module.exports = {
     },
     module: {
         rules: [
-            {
-                test: /\.njk$/,
-                use: [
-                    {
-                        loader: 'simple-nunjucks-loader',
-                        options: {}
-                    }
-                ]
-            }
+           
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'about.html',
-            template: './src/index.html'
-        }),
-        ...pages //pages[0], pages[1], ... 
+        })
     ],
 };
