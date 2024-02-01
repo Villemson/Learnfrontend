e5543ef21b7c4339bcb2d28c90d6687322c70a0c
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-02-01 17:37:22 +0200

    Add ItemList example

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,36 @@
 <script setup>
-import { ref } from 'vue';
-let count = ref(0);
+import { ref, computed } from 'vue';
+import ItemList from './ItemList.vue';
 let message = ref('Hello vue');
+let i = 0;
+let items = ref([
+    {id:i++, name: 'piim', isDone: true},
+    {id:i++, name: 'viin', isDone: false},
+    {id:i++, name: 'sai', isDone: true},
+]);
+function add(){
+    if(message.value.trim() !== ''){
+        items.value.push({id:i++, name: message.value.trim(), isDone: false});
+    }
+    message.value = '';
+}
+let doneItems = computed(() => {
+    return items.value.filter(item => item.isDone);
+});
+
+let toDoItems = computed(() => {
+    return items.value.filter(item => !item.isDone);
+});
 </script>
 
 <template>
     <div class="container mt-3">
-        <input class="input" v-bind:value="message">
-        <button class="button" v-on:click="count++">{{ count }}</button>
+        <input class="input" v-model="message" @keydown.enter="add">
+        <button class="button" @click="add">Add</button>
+        <div class="content">
+           <ItemList title="All Items" :items="items"></ItemList>
+           <ItemList title="Done Items" :items="doneItems"></ItemList>
+           <ItemList title="ToDo Items" :items="toDoItems"></ItemList>
+        </div>
     </div>
 </template>
---
+++ src/ItemList.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps(['title', 'items']);
+</script>
+
+<template>
+    <h1>All Items</h1>
+    <ul>
+        <li v-for="item in items" :key="item.id">
+            {{ item.name }}
+            <input type="checkbox" v-model="item.isDone">
+        </li>
+    </ul>
+</template>
