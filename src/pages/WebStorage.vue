084ca68f1b835e1430a99cb2ba74f260e41bea74
Marko Villemson <marko.villemson@tptlive.ee>
2024-02-18 14:12:54 +0200

    Updated WebStorage.vue for multiple name storage

--- src/pages/WebStorage.vue
+++ src/pages/WebStorage.vue
@@ -1,28 +1,31 @@
 <template>
-    <div>
-      <input v-model="kasutajaNimi" placeholder="Sisesta oma nimi">
-      <button @click="salvestaNimi">Salvesta Nimi</button>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        kasutajaNimi: '',
-      };
-    },
-    methods: {
-      salvestaNimi() {
-        localStorage.setItem('nimi', this.kasutajaNimi);
-        alert('Nimi salvestatud!');
-      }
-    },
-    mounted() {
-      if (localStorage.getItem('nimi')) {
-        this.kasutajaNimi = localStorage.getItem('nimi');
-      }
-    },
-  };
-  </script>
-  
+  <div>
+    <input v-model="kasutajaNimi" placeholder="Sisesta oma nimi">
+    <button @click="saveName">Salvesta Nimi</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      kasutajaNimi: '',
+    };
+  },
+  methods: {
+    saveName() {
+      let names = JSON.parse(localStorage.getItem('names')) || [];
+      names.push(this.kasutajaNimi);
+      localStorage.setItem('names', JSON.stringify(names));
+      alert('Nimi salvestatud!');
+      this.kasutajaNimi = '';
+    }
+  },
+  mounted() {
+    let names = JSON.parse(localStorage.getItem('names')) || [];
+    if (names.length > 0) {
+      this.kasutajaNimi = names[names.length - 1];
+    }
+  },
+};
+</script>
